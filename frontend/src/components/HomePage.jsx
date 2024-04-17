e28b8a404043b76aefdbfb0bd38103b9a831442f
alex_davila_alonso <alex.davilaalonso@dawsoncollege.qc.ca>
2024-04-17 13:49:06 -0400

    Read catalogs from endpoint

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,6 +1,55 @@
+import {atom, useAtom} from 'jotai';
+import {useEffect} from 'react';
+import axios from 'axios';
+
+const fetchCatalogs = () => {
+    const token = localStorage.getItem('token');
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+
+    return axios.get('http://localhost:3000/api/catalogs', config)
+    .then(response => {
+            if(response.status !== 200){
+                throw new Error(`API call failed with status ${response.data}`);
+            }
+            return response.data.content;
+    })
+    .catch(error => {
+        console.log(error);
+        return [];
+    })
+}
+
+const catalogAtom = atom([]);
+
 const HomePage = () => {
+    const [catalogs, setCatalogs] = useAtom(catalogAtom);
+
+    useEffect(() => {
+        if(localStorage.getItem('catalogs')){
+            setCatalogs(JSON.parse(localStorage.getItem('catalogs')));
+        }else{
+            fetchCatalogs().then(data => {
+                setCatalogs(data);
+                localStorage.setItem('catalogs',JSON.stringify(data));
+            });
+        }
+    },[])
+
     return (
-      <h1>Welcome To The Home Page</h1>
+        <>
+        <h1>Welcome To The Home Page</h1>
+        <div>
+                {catalogs ?
+                    catalogs.map(catalog => (
+                        <h1>{catalog.name}</h1>
+                    ))
+                    :
+                    <h1>No Catalogs!</h1>
+                }
+        </div>
+        </>
     );
 }
 
